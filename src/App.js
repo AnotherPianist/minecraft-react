546dc7577aa1f19b3259755c440cf123d4a28104
ThePianist <cafarimo@outlook.com>
2022-09-30 19:39:04 -0300

    add first person view

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 import { Ground } from './components/Ground';
 import { Player } from './components/Player';
+import { FPV } from './components/FPV';
 
 function App() {
   return (
     <Canvas>
       <Sky sunPosition={[100, 100, 20]} />
       <ambientLight intensity={0.5} />
+      <FPV />
       <Physics>
         <Player />
         <Ground />
